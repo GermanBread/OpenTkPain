d5c1d2b9d17e044f1f0f2a4c2bfd131cd89f3397
GermanBread <nd01@online.de>
2021-06-30 21:38:28 +0200

    add TODO and fix typo in readme

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 *My first attempt at creating a usable 2D game engine*
 
-I plan on using this in some of my future projects, though I will probably rewrite it sooner or later (for simplicity sake). *Perhaps a approach that is not object oriented?*
+I plan on using this in some of my future projects, though I will probably rewrite it sooner or later (for simplicity sake). *Perhaps an approach that is not object oriented?*
 
-You can view the development progress in [Program.cs](Program.cs)
+You can view the development progress [here](TODO)
--- uf.Tests/Program.cs
+++ uf.Tests/Program.cs
@@ -30,51 +30,6 @@ namespace uf
             catch (Exception ex) {
                 Logger.Log(new LogMessage(LogSeverity.Critical, "Something is wrong in the process!", ex));
             }
-
-            // Troubleshooting steps when something doesn't work:
-            // Make sure everything runs in one thread
-            
-            // TODO
-            // [DIFFICULTY] lower = easier; scale 1-10
-
-            // Working on it (when these two are done my engine is ready for use, just not feature-complete):
-            // [5] Animation system
-            // [8] Text
-            
-            // To be done
-            // [3] Make it easy to create .ufr archives
-            // [3] Add support for multiple atlases (will be needed for text rendering?)
-            // [6] Rework Audio: Split Music / Sound streams. FFT Sample should request blocks in size of 2^x
-            //!    Volume can be set with Bass.SampleSetInfo();
-            // [8] Add a class that does instanced rendering... Perhaps called "InstancedObject"?
-            //!    It should also check if all objects are of the same type... or it should draw the different types in a instanced render
-            // [5] Add ability to save / load texture atlases
-            // [5] Rewrite Transform.cs to feature less spaghetti
-            // [6] Make multithreading easy
-            //!    Provide a class that can be used to launch code in a seperate thread
-            //!    Race conditions should be avoided!
-            // [2] Move drawable objects list from BaseGame.cs to SceneManager.cs
-            // [3] Drawing & Multipass: Instead of invalidating the list when am element changes, remove/add that object/scene from the list.
-            // [?] Integrate ImGUI.Net
-            // [?] Migrate to OpenAL
-
-            // Done
-            // [3] Implement a file format for resources (perhaps .ufr?)
-            //!    These resource files should support containing more than one file, and even other resource files (dropped the nested resource thing)
-            // [2] Convert to library
-            // [3] Change LoadScene() to load the scenes indirectly. Instead it should be done by the main thread!
-            // [5] Parenting (might need recursion)
-            // [4] Textures (atlas)
-            // [3] Rewrite the entire Shader class
-            // [2] Multipass aka cursor detection
-            // [8] Fix some issues SPECIFIC TO WINDOWS (Accessviolation in Init() method. Most likely caused during texture load)
-            //!    2021-04-09: This is caused by a bug in Shader.cs (probably)
-            //!    2021-04-28: New issue: Context.MakeCurrent() fails because the resource is in use (._.)
-            // [3] Backport to older versions of OpenGL
-            // [3] Scene position offset, rotation, scale
-            // [2] Input helper (must also be able to determine the object clicked)
-            // [1] Camera (must be able to convert between camera-coordinates, world-coordinates and cursor-coordinates)
-            // [4] Texture atlas (applies to text too!) [DONE!]
         }
     }
 }
